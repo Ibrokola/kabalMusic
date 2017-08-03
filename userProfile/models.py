8f23589ed514cc73039ba01176454ccca421ec68
ibrobabs <babskolawole@gmail.com>
2017-08-03 00:51:09 -0600

    back at this

--- userProfile/models.py
+++ userProfile/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+
+
+User = get_user_model()
+
+class Album(models.Model):
+    '''The Album model which creates tables in the database for album objects storage'''
+    user = models.ForeignKey(User)
+    artist = models.CharField(max_length=500)
+    album_title = models.CharField(max_length=700)
+    genre = models.CharField(max_length=200)
+    album_image = models.FileFiedl()
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+
+class Song(models.Model):
+    '''The Song model which creates tables in the database for song objects storage'''
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song_title = models.CharField(max_length=250)
+    audo_file = models.FileField(default='')
+    is_favourite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_title
