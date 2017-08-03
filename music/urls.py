285923df37fda6286a91baa0392d822d437cde03
ibrobabs <babskolawole@gmail.com>
2017-08-03 02:33:15 -0600

    update

--- music/urls.py
+++ music/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     url(r'^(?P<album_id>[0-9]+)/$', AlbumDetailView.as_view(), name='album_detail'),
     url(r'^(?P<album_id>[0-9]+)/album_delete/$', AlbumDeleteView.as_view(), name='album_delete'),
     
-    url(r'^/(?P<album_id>[0-9]+)/song_create/$', SongCreateView.as_view(), name='song_create'),
-    url(r'^/songs/(?P<filter_by>[a-zA_Z]+)/$', SongListView.as_view(), name='song_list'),
-    url(r'^/(?P<album_id>[0-9]+)/delete_song/(?<song_id>[0-9]+)/$', SongDeleteView.as_view(), name='song_delete')
+    url(r'^(?P<album_id>[0-9]+)/song_create/$', SongCreateView.as_view(), name='song_create'),
+    url(r'^songs/(?P<filter_by>[a-zA_Z]+)/$', SongListView.as_view(), name='song_list'),
+    url(r'^(?P<album_id>[0-9]+)/delete_song/(?P<song_id>[0-9]+)/$', SongDeleteView.as_view(), name='song_delete')
 ]
